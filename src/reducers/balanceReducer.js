167b7dd7ba2718d6e348f13b00ef28bf2ca897a7
Hamdi MILED <hamdismiled@gmail.com>
2018-08-10 09:46:39 +0100

    add component Balance

---
+++ src/reducers/balanceReducer.js
@@ -0,0 +1,14 @@
+
+import * as constants from '../actions/constants';
+
+const balanceReducer = (state = 0, action) => {
+    //let balance;
+    switch (action.type) {
+        case (constants.SET_BALANCE):
+            return action.balance;
+            break;
+        default:
+            return balanceReducer;
+    }
+}
+export default balanceReducer;
